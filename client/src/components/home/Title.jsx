a267c98284c5e8677aed41c8e8dfbfd9aa949db9
mclhrd <rudmichael@outlook.com>
2025-10-26 21:13:55 -0400

    Progress on Personal Info, Dashboard and Resume Builder

--- client/src/components/home/Title.jsx
+++ client/src/components/home/Title.jsx
@@ -4,7 +4,7 @@ const Title = ({title, description}) => {
   return (
     <div className='text-center mt-6 text-slate-700'>
         <h2 className='text-4xl sm:text-4xl font-medium'>{title}</h2>
-        <p className='max-sm max-w-2x1 mt-4 text-slate-500'>{description}</p>
+        <p className='max-sm max-w-2xl mt-4 text-slate-500'>{description}</p>
     </div>
   )
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -36,6 +36,14 @@ const Dashboard = () => {
     event.preventDefault()
 
   }
+
+  const deleteResume = async (resumeId) => {
+    const confirm = window.confirm("Are you sure you want to delete this resume?")
+    if(confirm){
+      setAllResumes(prev => prev.filter(resume => resume._id !== resumeId))
+    }
+  }
+  
   useEffect(()=>{
     loadAllResumes()
   },[])
@@ -73,7 +81,7 @@ const Dashboard = () => {
                   </p>
 
                   <div onClick={e=>e.stopPropagation()} className='absolute top-1 right-1 group-hover:flex items-center hidden'>
-                    <TrashIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
+                    <TrashIcon onClick={()=>deleteResume(resume._id)} className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
                     <PencilIcon onClick={()=>{setEditResumeId(resume._id); setTitle(resume.title)}} className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
                   </div>
 
